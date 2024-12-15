740dc2407500744248aed868c1a2d103e8d14ea8
Chandrakant Pawar <137754266+pawarchandrakant29@users.noreply.github.com>
2024-12-15 21:52:39 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,14 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//     baseURL: "http://localhost:5000",
+// });
+
+// export default api;
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://inventory-management-vbji.onrender.com",
 });
 
 export default api;
